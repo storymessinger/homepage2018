f4575c148af98eda4f42c9dc693a2ab58a40479d
박준동 <jundong@bagjundongs-MacBook-Pro.local>
2018-02-11 22:09:29 +0900

    change

--- server.js
+++ server.js
@@ -12,5 +12,20 @@ app.get('/*', function(req, res) {
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
